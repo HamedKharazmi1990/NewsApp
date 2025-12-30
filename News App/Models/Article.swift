cdddf589854bae054915c84ba3167d032633a0a9
hamed kharazmi <hamed.kharazmi@bandpey.com>
2025-12-30 13:16:11 +0100

    Added readme and gitignore

--- News App/Models/Article.swift
+++ News App/Models/Article.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+// MARK: - Article
+struct Article: Codable {
+    let data: [Datum]
+}
+
+// MARK: - Datum
+struct Datum: Codable, Identifiable {
+    var id = UUID()
+    
+    let title: String
+    let description: String
+    let url: String
+    let imageURL: String
+    let publishedAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title, description, url
+        case imageURL = "image_url"
+        case publishedAt = "published_at"
+    }
+}
