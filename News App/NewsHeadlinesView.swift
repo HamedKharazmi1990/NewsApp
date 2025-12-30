b712506be2dba0b24934a9e616eeb4ab3c8be8d7
hamed kharazmi <hamed.kharazmi@bandpey.com>
2025-12-30 16:09:58 +0100

    ui cleanup and adding pull to refrsh

--- News App/NewsHeadlinesView.swift
+++ News App/NewsHeadlinesView.swift
@@ -15,28 +15,38 @@ struct NewsHeadlinesView: View {
             ZStack {
                 BackgroundView()
                 
-                if vm.isLoading {
-                    ProgressView("Loading...")
-                } else {
-                    List {
-                        ForEach(vm.newsModel.data) { newsModel in
-                            // TODO: Article Row View
-                            NavigationLink {
-                                WebView(URL(string: newsModel.url)!)
-                            } label: {
-                                ArticleRowView(datum: newsModel)
-                            }
+                // MARK: List of Articles
+                List {
+                    ForEach(vm.newsModel.data) { newsModel in
+                        // TODO: Article Row View
+                        NavigationLink {
+                            WebView(URL(string: newsModel.url)!)
+                        } label: {
+                            ArticleRowView(datum: newsModel)
                         }
                     }
+                }
+                
+                if vm.isLoading {
+                    ProgressView("Loading...")
+                        .font(.headline)
+                        .padding()
+                        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16))
+                        .shadow(radius: 10)
                 }
             }
             .alert("Error", isPresented: .constant(vm.errorMessage != nil)) {
                 Button("OK") {
                     vm.errorMessage = nil
                 }
+            } message: {
+                Text(vm.errorMessage ?? "")
             }
             .navigationTitle("Top Headlines")
             .task {
+                await vm.fetchHeadlines()
+            }
+            .refreshable {
                 await vm.fetchHeadlines()
             }
         }
